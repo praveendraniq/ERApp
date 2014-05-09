1b897fe5360befcf45cf4ae0a5c73a219204e766
gxpo <geninaapo@gmail.com>
2014-05-09 12:51:44 -0700

    done with addExpense() for ExpR

--- src/org/sjsucmpe131/model/ExpenseReport.java
+++ src/org/sjsucmpe131/model/ExpenseReport.java
@@ -8,27 +8,32 @@ public class ExpenseReport extends ParseObject {
 	public ExpenseReport() {
 
 	}
-	
+
 	/**
-	 * Returns true if submitted Expense Report is Complete meaning it was saved in background
-	 * Returns false if EXR is not complete, EXR is ready for retrieval
+	 * Returns true if submitted Expense Report is Complete meaning it was saved
+	 * in background Returns false if EXR is not complete, EXR is ready for
+	 * retrieval
 	 */
-	public boolean addExpense(TouchViewImageView photoImage, boolean isComplete, String price, String )
-	{
+	public boolean addExpense(TouchViewImageView photoImage,
+			boolean isComplete, String price, String merchant,
+			String description, String date, String comment, String currency,
+			String category, String payment) {
+		
+		
 		put("photoImage", photoImage);
 		// The photo is the optional problem.
 		// parse will query if photoimage is null
 		if (photoImage != null) {
 			put("isComplete", true);
 		}
-		put("price", price.getText());
-		put("merchant", merchant.getText());
-		put("description", description.getText());
-		put("date", date.getText());
-		put("comment", comment.getText());
-		put("currency", currency.getSelectedItem().toString());
-		put("category", category.getSelectedItem().toString());
-		put("payment", payment.getSelectedItem().toString());
+		put("price", price);
+		put("merchant", merchant);
+		put("description", description);
+		put("date", date);
+		put("comment", comment);
+		put("currency", currency);
+		put("category", category);
+		put("payment", payment);
 		saveInBackground();
 	}
 
