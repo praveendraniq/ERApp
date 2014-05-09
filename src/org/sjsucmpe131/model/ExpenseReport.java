459c62ba311ed889b82909235fecca83b96469e4
gxpo <geninaapo@gmail.com>
2014-05-09 12:06:13 -0700

    For MVC Pattern; ExpenseReport Object extends the ParseObject Class thus any time you make an ExpenseReport, it will have Parse functions

---
+++ src/org/sjsucmpe131/model/ExpenseReport.java
@@ -0,0 +1,13 @@
+package org.sjsucmpe131.model;
+
+import com.parse.ParseClassName;
+import com.parse.ParseFile;
+import com.parse.ParseObject;
+
+@ParseClassName("ExpenseReport")
+public class ExpenseReport extends ParseObject {
+	public ExpenseReport() {
+
+	}
+
+}
